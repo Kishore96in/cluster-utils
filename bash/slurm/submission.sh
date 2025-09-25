c671edb66f67ce228d15ea930c11fe557f7fe478
Kishore G. <kishore96.work@gmail.com>
2025-09-25 17:05:17 +0530

    Move scheduler-specific stuff to respective include files
    So that the user no longer needs to modify the contents of the repo
    itself.

--- bash/slurm/submission.sh
+++ bash/slurm/submission.sh
@@ -1,3 +1,11 @@
+function create_dependency {
+	#Argument to be passed to _submit_batch to create a dependency on a job (ID of the latter passed as argument)
+	#To be used without quoting the output
+	echo "--dependency=afterok:$1"
+	}
+
+_submit_batch="sbatch" #this will be used unquoted in some contexts.
+
 function get_workdir_for_jobid {
 	scontrol show job $@ | grep WorkDir | cut -d '=' -f 2
 	}
