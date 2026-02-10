e56f0b8f079447b212301d47aaa951aebb587876
Kishore G. <kishore96.work@gmail.com>
2026-02-10 14:47:27 +0530

    slurm/jint: bash -l

--- bash/slurm/submission.sh
+++ bash/slurm/submission.sh
@@ -1,5 +1,5 @@
 alias jdel='scancel'
-alias jint='salloc --ntasks=1 srun --pty bash'
+alias jint='salloc --ntasks=1 srun --pty bash -l'
 
 function create_dependency {
 	#Argument to be passed to _submit_batch to create a dependency on a job (ID of the latter passed as argument)
