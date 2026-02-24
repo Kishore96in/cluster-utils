f8148d6eeb8f5dba95a770e18cc1874e4b41734b
Kishore G. <kishore96.work@gmail.com>
2026-02-24 12:41:46 +0530

    slurm: output username in ja etc.

--- bash/slurm/monitoring.sh
+++ bash/slurm/monitoring.sh
@@ -1,8 +1,8 @@
 #Aliases and functions to help monitor PBS jobs
 
-alias ja="squeue --format='%.6i  %.2t %.9L %.5C' " #All jobs
-alias jaq='squeue --format="%.6i  %.2t %.9L %.5C" | grep PD' #All queued jobs
-alias jar='squeue --format="%.6i  %.2t %.9L %.5C" | grep R' #All running jobs
+alias ja="squeue --format='%.6i  %.2t %.9L %.5C  %u' " #All jobs
+alias jaq='squeue --format="%.6i  %.2t %.9L %.5C  %u" | grep PD' #All queued jobs
+alias jar='squeue --format="%.6i  %.2t %.9L %.5C  %u" | grep R' #All running jobs
 alias myq="squeue --format='%.6i  %.2t %.9L %.5C'-u $USER" #All jobs belonging to the current user
 alias jinfo='scontrol show job' #Get information about a job ID
 
